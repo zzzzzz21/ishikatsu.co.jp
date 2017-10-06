bcb1d878880dcda7d84f89e1265301974911d2e5
kurahara <kurahara@unitea.jp>
2017-10-06 21:03:41 +0900

    初期コミット

---
+++ htdocs/share/js/fn.js
@@ -0,0 +1,50 @@
+var isSP = false;
+if (typeof(window.matchMedia) == "function") {
+  var widthQuery = window.matchMedia("(max-width:767px)");
+  if (typeof (widthQuery.matches) != "undefined" && widthQuery.matches == true) {
+    isSP = true;
+  }
+}
+/* トップページのカルーセル設定 */
+$(document).ready(function(){
+	if (isSP == true) {
+		$(".carousel__image").each(function(){
+			$(this).attr("src", $(this).attr("src").replace('.png', '_sp.png'));
+		});
+	}
+    $('.nav__item').hover(function(){
+        $(this).parent().parent().parent().parent().toggleClass('header--active');
+    });
+	$('a[href^="#"]').click(function(){
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, 250, "swing");
+		return false;
+	});
+    var toTopButton = $('.footer__button');
+    toTopButton.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 490) {
+        toTopButton.fadeIn();
+        } else {
+        toTopButton.fadeOut();
+        }
+    });
+	if (isSP == true) {
+    	$('.nav__item').click(function() {
+            if ($(this).hasClass('nav__item--open')) {
+                $(this).removeClass('nav__item--open');
+            } else {
+                $('.nav__item').removeClass('nav__item--open');
+                $(this).toggleClass('nav__item--open');
+            }
+    	});
+    }
+	$('.header__button').click(function() {
+        $(this).toggleClass('header__button--open');
+		$(this).parent().next().fadeToggle('fast');
+		$('.megaDrop').removeClass('.megaDrop--open');
+		$('.nav__item').removeClass('nav__item--open');
+	});
+});
